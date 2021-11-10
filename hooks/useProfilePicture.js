fa3da4b3eda5984d223d856b18041cd2c36a7061
donalbertico <atates@udlanet.ec>
2021-11-10 00:11:47 +0000

    allowing image upload

---
+++ hooks/useProfilePicture.js
@@ -0,0 +1,25 @@
+import * as React from 'react'
+import * as firebase from 'firebase'
+
+export default function useProfilePicture(){
+  const [url,setUrl] = React.useState()
+  const [resolved, setResolved] = React.useState()
+
+  React.useEffect(() => {
+    console.log(url);
+    if(!url) setResolved(false)
+    if(url?.split('-profile')[1]){
+      firebase.storage().ref(url)
+      .getDownloadURL()
+      .then( fsUrl => {
+        setResolved(fsUrl)
+        console.log(fsUrl);
+      })
+      .catch(e => {console.log('error',e)})
+    }else {
+      setResolved(url)
+    }
+  },[url])
+
+  return [resolved,setUrl]
+}
--- screens/profile/EditScreen.js
+++ screens/profile/EditScreen.js
@@ -1,50 +1,98 @@
 import * as React from 'react'
 import * as firebase from 'firebase'
+import Toast from 'react-native-toast-message'
+import * as ImagePicker from 'expo-image-picker'
 import { View, ActivityIndicator, TouchableOpacity} from 'react-native'
 import { Input, Text, Button, Image} from 'react-native-elements'
 import {styles} from '../styles'
 import useUserStore from '../../hooks/useUserStore'
 import useUserRead from '../../hooks/useUserRead'
 import useAssetStore from '../../hooks/useAssetStore'
+import useProfilePicture from '../../hooks/useProfilePicture'
 
 import NavBar from '../components/bottomNavComponent'
 import Logout from '../auth/components/logoutComponent'
 
 export default function EditScreen(props){
   const [email,setEmail] = React.useState('')
-  const [name,setName] = React.useState('')
+  const [profilePicture,setUrl] = useProfilePicture()
+  const [firstName,setFirstName] = React.useState('')
+  const [lastName,setLastName] = React.useState('')
   const [assets,setAssets] = useAssetStore()
   const [avatarUri,setAvatar] = React.useState()
   const [loading,setLoading] = React.useState(false)
+  const [userRef,setUserRef] = React.useState()
   const [user] = useUserRead('get')
   const [setUser] = useUserStore()
 
-  handleEdit = ()=> {
+  const uploadPicture = () => {
+    async function pickImage() {
+      const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync()
+      if ( status == 'granted') {
+        let result = await ImagePicker.launchImageLibraryAsync({
+          mediaTypes: ImagePicker.MediaTypeOptions.Images,
+          aspect: [4, 3],
+          quality: 1
+        })
+        setUrl()
+        if(!result.cancelled) {
+          let picture = `profiles/${user.uid}-profile.jpg`
+          let file = await fetch(result.uri)
+          let blob = await file.blob()
+          firebase.storage()
+              .ref(picture)
+              .put(blob)
+              .then(() => {
+                userRef.update({picture : picture})
+                  .then(()=>{
+                    setUser({...user,picture : picture})
+                    setUrl(picture)
+                    setLoading(false)
+                  })
+                  .catch((e) => {
+                    setLoading(false)
+                    Toast.show({text1:'Error',
+                      text2: e.message ,
+                      type : 'error',
+                      position : 'bottom',
+                      visibilityTime: 4000})
+                  })
+              })
+        }
+      }
+    }
     setLoading(true)
-    let db = firebase.firestore();
-    let ref = db.collection('users').doc(user.uid)
-    const authUser = firebase.auth().currentUser
-    let newInfo = {description : 'ploplo'}
-    ref.update(newInfo)
+    pickImage()
+  }
+  const handleEdit = ()=> {
+    setLoading(true)
+    let newInfo = {firstName : firstName, lastName : lastName}
+    userRef.update(newInfo)
       .then(()=>{
-        authUser.updateProfile({
-              displayName : name
-            })
-            .then(()=>{
-              let update = {
-                displayName : name
-              }
-              setUser(Object.assign(user,update,newInfo))
-              props.navigation.navigate('home',{userUpdate : true})
-              setLoading(false)
-            },(e)=>{
-              setLoading(false)
-            })
+        setUser({...user,...newInfo})
+        props.navigation.navigate('home',{userUpdate : true})
+      })
+      .catch((e) => {
+        setLoading(false)
+        Toast.show({text1:'Error',
+          text2: e.message ,
+          type : 'error',
+          position : 'bottom',
+          visibilityTime: 4000})
       })
   }
 
   React.useEffect(() => {
-    setName(user.displayName)
+    setFirstName(user.firstName)
+    setLastName(user.lastName)
+    if(user.uid) {
+      let db = firebase.firestore();
+      setUserRef(db.collection('users').doc(user.uid))
+    }
+    if(user.picture){
+      console.log('cambia?');
+      setUrl(user.picture)
+    }
   },[user])
   React.useEffect(() => {
     if(assets) setAvatar(assets.avatar)
@@ -59,7 +107,9 @@ export default function EditScreen(props){
               <View style={{flex:1}}></View>
               <View style={{flex:8}}>
                 <View style={styles.horizontalView}>
-                  <Image style={styles.roundImage} source={{uri: user.picture? user.picture : avatarUri}}/>
+                  <TouchableOpacity onPress={uploadPicture}>
+                    <Image style={styles.roundImage} source={{uri: profilePicture? profilePicture: avatarUri}}/>
+                  </TouchableOpacity>
                   <View style={{flex:1}}></View>
                   <View>
                     {!user.provider && (
@@ -74,7 +124,8 @@ export default function EditScreen(props){
                   <View style={styles.horizontalView}>
                     <View style={{flex:1,marginBottom:50}}></View>
                   </View>
-                  <Input placeholder='name' value={name} onChangeText={(name)=>setName(name)}></Input>
+                  <Input placeholder='first name' value={firstName} onChangeText={(name)=>setFirstName(name)}></Input>
+                  <Input placeholder='last name' value={lastName} onChangeText={(lastname)=>setLastName(lastname)}></Input>
                   <Button title='edit' onPress={handleEdit}/>
                 </View>
               </View>
